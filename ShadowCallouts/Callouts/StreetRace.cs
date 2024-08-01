299cf24a2854218c7fab227fe25fc184bf81df77
Anonymous <87971370+anonymous-1001@users.noreply.github.com>
2024-08-01 10:02:37 +0530

    Add files via upload

---
+++ ShadowCallouts/Callouts/StreetRace.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Rage;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+
+namespace ShadowCallouts.Callouts
+{
+    [CalloutInfo("StreeRace", CalloutProbability.High)]
+    public class StreetRace : Callout
+    {
+        private Ped Suspect;
+        private Vehicle SuspectVehicle;
+        private Blip SuspectBlip;
+        private LHandle Pursiut;
+        private Vector3 Spawnpoint;
+        private bool PursuitCreated;
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+            Spawnpoint = World.GetRandomPositionOnStreet();
+            ShowCalloutAreaBlipBeforeAccepting(Spawnpoint, 30f);
+            AddMinimumDistanceCheck(30f, Spawnpoint);
+            CalloutMessage = "Street Race in Pursuit";
+            CalloutPosition = Spawnpoint;
+            Functions.PlayScannerAudioUsingPosition("WE_HAVE_CRIME_RESISTING_ARREST_02 IN_OR_ON_POSITION", Spawnpoint);
+
+            return base.OnBeforeCalloutDisplayed();
+        }
+
+        public override bool OnCalloutAccepted()
+        {
+            SuspectVehicle = new Vehicle("BALLER", Spawnpoint);
+            SuspectVehicle.IsPersistent = true;
+
+            Suspect = new Ped(SuspectVehicle.GetOffsetPositionFront(5f));
+            Suspect.IsPersistent = true;
+            Suspect.BlockPermanentEvents = true;
+            Suspect.WarpIntoVehicle(SuspectVehicle, -1);
+
+            SuspectBlip = Suspect.AttachBlip();
+            SuspectBlip.Color = System.Drawing.Color.Yellow;
+            SuspectBlip.IsRouteEnabled = true;
+
+            PursuitCreated = false;
+
+            return base.OnCalloutAccepted();
+        }
+
+        public override void Process()
+        {
+            base.Process();
+
+            if (!PursuitCreated && Game.LocalPlayer.Character.DistanceTo(SuspectVehicle) <= 20f)
+            {
+                Pursiut = Functions.CreatePursuit();
+                Functions.AddPedToPursuit(Pursiut, Suspect);
+                Functions.SetPursuitIsActiveForPlayer(Pursiut, true);
+                PursuitCreated = true;
+            }
+
+            if (PursuitCreated && !Functions.IsPursuitStillRunning(Pursiut))
+            {
+                End();
+            }
+        }
+
+        public override void End()
+        {
+            base.End();
+
+            if (Suspect.Exists())
+            {
+                Suspect.Dismiss();
+            }
+            if (SuspectBlip.Exists())
+            {
+                SuspectBlip.Delete();
+            }
+            if (SuspectVehicle.Exists())
+            {
+                SuspectVehicle.Dismiss();
+            }
+
+            Game.LogTrivial("ShadowCallouts - Street Race Cleaned up");
+        }
+    }
+}
---
+++ ShadowCallouts/IniFile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SecurityPlus
+{
+    /// <summary>
+    /// Create a New INI file to store or load data
+    /// </summary>
+    public class IniFile
+    {
+        public string path;
+
+        [DllImport("kernal32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernal32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        /// <summary>
+        /// INIFile Constructor
+        /// </summary>
+        /// <PARAM name="INIPath"></PARAM>
+        public IniFile(string INIPath)
+        {
+            path = INIPath;
+        }
+        /// <summary>
+        /// Write Data to the INI file
+        /// </summary>
+        /// <PARAM name="Section"></PARAM>
+        /// Key Name
+        /// <PARAM name="Value"></PARAM>
+        /// Value Name
+        public void IniWriteValue(string Section, string key, string Value)
+        {
+            WritePrivateProfileString(Section, key, Value, this.path);
+        }
+
+        /// <summary>
+        /// Read Data Value From the Ini file
+        /// </summary>
+        /// <PARAM name="Section"></PARAM>
+        /// <PARAM name="Key"></PARAM>
+        /// <PARAM name="Path"></PARAM>
+        /// <return></return>
+        public string IniReadValue(string Section, string key)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(Section, key, "", temp, 255, this.path);
+            return temp.ToString();
+        }
+    }
+
+}
